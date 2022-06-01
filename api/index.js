18dae9f4bf284579576aeb8877db257b29d3a7c1
Breno Mazieiro <breno.mazieiro@gmail.com>
2022-06-01 13:56:38 +0200

    Merge
    Co-authored-by: Aline Galdino <AlineGaldino@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
-import Apollo from 'apollo-server';
+import Apollo, { ApolloError } from 'apollo-server';
 import {
   ApolloServerPluginLandingPageGraphQLPlayground
 } from "apollo-server-core";
 
+
+/**
+ * typeDefs: GraphQL schema definicao dos tipos de dados
+ * resolvers: Funções que manipulam os dados
+ * input: cliente envia para o server
+ * type: server envia pro cliente
+ */
 
 const typeDefs = `#graphql
   type User {
@@ -21,27 +28,31 @@ const typeDefs = `#graphql
   }
 
   type Mutation {
-    MergeUser(id: ID, input: UserData): User
+    MergeUser(id: ID, deleteIt: Boolean, input: UserData): User
   }
 `
-const users = [
-  { id: '1', name: 'John', age: 30 },
-  { id: '2', name: 'Jane', age: 20 }
+let users = [
+  { id: 0, name: 'John', age: 30 },
+  { id: 1, name: 'Jane', age: 20 }
 ];
 
 const resolvers = {
   Query: {
     users: (_, { id }) => {
       if (id) {
-        return users.filter(user => {
-          return user.id === id
+        const user = users.filter(user => {
+          return user.id === Number(id);
         })
+        if (!user.length) {
+          throw new ApolloError('User not found');
+        }
+        return user;
       };
       return users;
     }
   },
   Mutation: {
-    MergeUser: (parent, args, context, info) => {
+    MergeUser: (parent, { id, deleteIt, input }, context, info) => {
       return null;
     }
   }
@@ -53,6 +64,19 @@ const server = new Apollo.ApolloServer({
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground(),
   ],
+  formatError: (error) => {
+    // eslint-disable-next-line no-param-reassign
+    delete error.extensions.exception.stacktrace
+    const errorlog = {
+      message: error.message,
+      code: error.extensions.code,
+      Detail: error.extensions.exception ? error.extensions.exception : null,
+      Path: error.path ? error.path[0] : null,
+    }
+    // eslint-disable-next-line no-console
+    console.error('\x1b[31m%s\x1b[0m', `[${'s'}] ERROR: Server: ${JSON.stringify(errorlog)}`)
+    return errorlog
+  },
 });
 
 const serverInit = () => {
